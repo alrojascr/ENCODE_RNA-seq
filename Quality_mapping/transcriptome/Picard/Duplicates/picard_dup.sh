e17d08bc7abdf15d1b2ae270f90968594accf92b
Alexis Felipe Rojas Cruz <86197200+alrojascr@users.noreply.github.com>
2026-04-03 17:22:52 -0500

    Update picard_dup.sh

--- Quality_mapping/transcriptome/Picard/Duplicates/picard_dup.sh
+++ Quality_mapping/transcriptome/Picard/Duplicates/picard_dup.sh
@@ -2,57 +2,42 @@
 #SBATCH --job-name=Picard
 #SBATCH --output=Picard_%j.out
 #SBATCH --error=Picard_%j.err
-#SBATCH --exclude=pujnodo0
-#SBATCH --nodes=2
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
-#SBATCH --partition=debug
+#SBATCH --nodelist=nodei-10
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=150G
+#SBATCH --time=15-00:00:00
+#SBATCH --partition=long
 
 # Capture start time
 start_time=$(date +%s)
 
-# Input and output directories
-input_dir="/opt/data/HPC01A/alexis_rojasc/OSA_RNA-seq/Quality_mapping/transcriptome/Samtools/sort"
-output_base_dir="/opt/data/HPC01A/alexis_rojasc/OSA_RNA-seq/Quality_mapping/transcriptome/Picard/Duplicates"
+input_dir="/hpcfs/home/cursos/s.abril01/RNA-seq/Odon/Quality_mapping/Samtools/sort"
+output_base_dir="/hpcfs/home/cursos/s.abril01/RNA-seq/Odon/Quality_mapping/Picard"
 
-# Create output directory for read group and deduplicated files
-mkdir -p "${output_base_dir}/read_groups"
-mkdir -p "${output_base_dir}/dedup"
+mkdir -p "${output_base_dir}/Results"
 
 for bam_file in ${input_dir}/*.bam; do
     base_name=$(basename "$bam_file" .bam)
     base_name="${base_name%%_*}"
 
-    # Set the output BAM files for read groups and deduplication
-    read_group_bam="${output_base_dir}/read_groups/${base_name}_readgroup.bam"
-    dedup_bam="${output_base_dir}/dedup/${base_name}_dedup.bam"
-    metrics_file="${output_base_dir}/dedup/${base_name}_metrics.txt"
+    # Set the output BAM files for deduplication
+    dedup_bam="${output_base_dir}/Results/${base_name}_dedup.bam"
+    metrics_file="${output_base_dir}/Results/${base_name}_metrics.txt"
     
-    echo "Adding read groups to $bam_file into $read_group_bam"
-
-    # Add or replace read groups using Picard
-    picard AddOrReplaceReadGroups \
-        -Xmx20G \
-        I="$bam_file" \
-        O="$read_group_bam" \
-        RGID="$base_name" \
-        RGLB="lib1" \
-        RGPL="Illumina" \
-        RGPU="unit1" \
-        RGSM="$base_name"
-
-    echo "Marking duplicates for $read_group_bam into $dedup_bam"
+    echo "Marking duplicates for $bam_file into $dedup_bam"
 
     # Mark duplicates using Picard with the specified files
     picard MarkDuplicates \
-        -Xmx20G \
-        I="$read_group_bam" \
+        -Xmx140G \
+        I="$bam_file" \
         O="$dedup_bam" \
         M="$metrics_file" \
         ASSUME_SORTED=true \
-        REMOVE_DUPLICATES=false
+        REMOVE_DUPLICATES=false \
+        CREATE_INDEX=true
 
 done
 
